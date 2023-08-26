98a9e9b8b0a0479689a65e0b713593b930e1027a
[Alex Samal] <[alex.samal127@gmail.com]>
2023-08-26 19:42:47 +0300

    add routing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express, {Request, Response} from "express";
+import bodyParser from "body-parser"
+import {blogsRouter} from "./routers/blogs-router";
+import {postsRouter} from "./routers/posts-router";
 
 const app = express()
 const port = 3000
 
+const parserMiddleware = bodyParser()
+app.use(parserMiddleware)
 app.use(express.json())
+
+app.use("/blogs", blogsRouter)
+app.use("/posts", postsRouter)
 
 app.listen(port, () => {
     console.log(`Example app listenning on port ${port}`)
