1475d1af88c816f3f4d5c73c9d6bd06f75aa1bff
[Alex Samal] <[alex.samal127@gmail.com]>
2023-08-28 18:41:12 +0300

    add testingRouter index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import bodyParser from "body-parser"
 import {blogsRouter} from "./routers/blogs-router";
 import {postsRouter} from "./routers/posts-router";
 import {testingRouter} from "./routers/testing-router";
+import {db} from "./db";
 
 const app = express()
 const port = 3000
@@ -15,6 +16,11 @@ app.use(express.json())
 app.use("/testing/all-data",testingRouter)
 app.use("/blogs", blogsRouter)
 app.use("/posts", postsRouter)
+
+app.delete("/testing/all-data", (req: express.Request, res: express.Response) => {
+    db.blogs.length = 0;
+    db.posts.length = 0;
+});
 
 
 app.listen(port, () => {
