867ed1fc8948c69949383dbb87f6ef2af9a03b64
[Alex Samal] <[alex.samal127@gmail.com]>
2023-08-28 18:03:45 +0300

    add testingRouter index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser"
 import {blogsRouter} from "./routers/blogs-router";
 import {postsRouter} from "./routers/posts-router";
+import {testingRouter} from "./routers/testing-router";
 
 const app = express()
 const port = 3000
@@ -12,6 +13,7 @@ app.use(express.json())
 
 app.use("/blogs", blogsRouter)
 app.use("/posts", postsRouter)
+app.use("/all-data",testingRouter)
 
 app.listen(port, () => {
     console.log(`Example app listenning on port ${port}`)
