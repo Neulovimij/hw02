3c737d315ceb1e92a9162a62dde7821a350a4954
[Alex Samal] <[alex.samal127@gmail.com]>
2023-08-31 19:14:46 +0300

    fix some bugs in routers

--- src/middlewares/posts-validation.ts
+++ src/middlewares/posts-validation.ts
@@ -4,9 +4,9 @@ import {inputValidatorMiddleware} from "./input-validation-middleware";
 
 
 export const validationCreateUpdatePost = [
-    body('title').isString().trim().isLength({min: 1, max: 30}).withMessage('incorrect title'),
-    body('shortDescription').isString().trim().isLength({min: 1, max: 100}).withMessage('incorrect shortDescription'),
-    body('content').isString().trim().isLength({min: 1, max: 500}).withMessage('incorrect content'),
+    body('title').isString().trim().isLength({min: 1, max: 30}).withMessage('incorrect title (maxLength: 30)'),
+    body('shortDescription').isString().trim().isLength({min: 1, max: 100}).withMessage('incorrect shortDescription (maxLength: 100)'),
+    body('content').isString().trim().isLength({min: 1, max: 500}).withMessage('incorrect content (maxLength: 500)'),
     body('blogId').isString().trim().notEmpty().withMessage('incorrect blogId').custom((id,req ) => {
         const postUpdate = db.blogs.find(b => b.id === id)
         if (!postUpdate) {
