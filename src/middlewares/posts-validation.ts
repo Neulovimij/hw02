c86d3904de127e22745d6eca13dc154e315101a9
[Alex Samal] <[alex.samal127@gmail.com]>
2023-08-31 18:54:37 +0300

    fix some bugs in routers

--- src/middlewares/posts-validation.ts
+++ src/middlewares/posts-validation.ts
@@ -7,7 +7,7 @@ export const validationCreateUpdatePost = [
     body('title').isString().trim().isLength({min: 1, max: 30}).withMessage('incorrect title'),
     body('shortDescription').isString().trim().isLength({min: 1, max: 200}).withMessage('incorrect shortDescription'),
     body('content').isString().trim().isLength({min: 1, max: 500}).withMessage('incorrect content'),
-    body('blogId').isString().trim().notEmpty().withMessage('incorrect blogId').custom((id,{req} ) => {
+    body('blogId').isString().trim().notEmpty().withMessage('incorrect blogId').custom((id,req ) => {
         const postUpdate = db.blogs.find(b => b.id === id)
         if (!postUpdate) {
             throw new Error("blog is not found")
